a81d553834c3c662e2dec1c44fb8c29751726c2f
Scott Harrington <robertscottharrington@gmail.com>
2022-09-14 18:19:44 -0400

    added color changing heading

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const heading = document.querySelector('h1')
+const heading = document.querySelector("h1");
 
-
-setInterval(function() {
-    console.log(Math.floor(Math.random() * 255));
-}, 1000)
+function rgbString() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+setInterval(function () {
+  heading.style.color = rgbString();
+}, 1000);
